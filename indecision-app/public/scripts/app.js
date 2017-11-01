fba86c2d1027afb85bb68008267dc020334cd900
Mohammad Najar <mmotieian@yahoo.com>
2017-11-01 11:54:39 -0500

    arrays in react

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -5,66 +5,82 @@ console.log('App.js is running!');
 var app = {
     title: 'Indecision App',
     subtitle: 'Put your life in the hands of a computer',
-    options: ['One', 'Two']
+    options: []
 };
 
-var template = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        app.title
-    ),
-    app.subtitle && React.createElement(
-        'p',
-        null,
-        app.subtitle
-    ),
-    React.createElement(
-        'p',
-        null,
-        app.options && app.options.length > 0 ? 'Here are your options' : 'No Options'
-    )
-);
+var onFormSubmit = function onFormSubmit(e) {
+    e.preventDefault();
 
-// --------------------------------------------
+    var option = e.target.elements.option.value;
 
-var user = {
-    name: 'Mohammad N',
-    age: 33,
-    location: 'Plano'
+    if (option) {
+        app.options.push(option);
+        e.target.elements.option.value = '';
+        render();
+    }
 };
 
-function getLocation(location) {
+var onRemoveAll = function onRemoveAll() {
+    app.options = [];
+    render();
+};
 
-    if (location) {
-        return React.createElement(
+var appRoot = document.getElementById('app');
+
+var render = function render() {
+    var template = React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'h1',
+            null,
+            app.title
+        ),
+        app.subtitle && React.createElement(
             'p',
             null,
-            'Location: ',
-            location
-        );
-    }
-}
+            app.subtitle
+        ),
+        React.createElement(
+            'p',
+            null,
+            app.options.length > 0 ? 'Here are your options' : 'No options'
+        ),
+        React.createElement(
+            'p',
+            null,
+            app.options.length
+        ),
+        React.createElement(
+            'button',
+            { onClick: onRemoveAll },
+            'Remove All'
+        ),
+        React.createElement(
+            'ol',
+            null,
+            app.options.map(function (option) {
+                return React.createElement(
+                    'li',
+                    { key: option },
+                    'Item ',
+                    option
+                );
+            })
+        ),
+        React.createElement(
+            'form',
+            { onSubmit: onFormSubmit },
+            React.createElement('input', { type: 'text', name: 'option' }),
+            React.createElement(
+                'button',
+                null,
+                'Add Option'
+            )
+        )
+    );
 
-var templateTwo = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        user.name ? user.name.toUpperCase() : undefined
-    ),
-    user.age && user.age >= 18 && React.createElement(
-        'p',
-        null,
-        'Age: ',
-        user.age
-    ),
-    getLocation(user.location)
-);
+    ReactDOM.render(template, appRoot);
+};
 
-// --------------------------------------------
-
-ReactDOM.render(template, document.getElementById('app'));
+render();
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -3,41 +3,51 @@ console.log('App.js is running!');
 const app = {
     title: 'Indecision App',
     subtitle: 'Put your life in the hands of a computer',
-    options: ['One', 'Two']
+    options: []
 };
 
-const template = (
-    <div>
-        <h1>{app.title}</h1>
-        {app.subtitle && <p>{app.subtitle}</p>}
-        <p>{ (app.options && app.options.length > 0 ) ? 'Here are your options' : 'No Options'}</p>
-    </div>
-);
+const onFormSubmit = (e) => {
+    e.preventDefault();
 
-// --------------------------------------------
+    const option = e.target.elements.option.value;
 
-const user = {
-    name: 'Mohammad N',
-    age: 33,
-    location: 'Plano'
-}
-
-function getLocation(location) {
-
-    if (location) {
-        return <p>Location: {location}</p>;
+    if (option) {
+        app.options.push(option);
+        e.target.elements.option.value = '';
+        render();
     }
-}
+};
 
+const onRemoveAll = () => {
+    app.options = [];
+    render();
+};
 
-const templateTwo = (
-    <div>
-        <h1>{user.name ? user.name.toUpperCase() : undefined}</h1>
-        {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
-        {getLocation(user.location)}
-    </div>
-);
+const appRoot = document.getElementById('app');
 
-// --------------------------------------------
+const render = () => {
+    const template = (
+        <div>
+            <h1>{app.title}</h1>
+            {app.subtitle && <p>{app.subtitle}</p>}
+            <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+            <p>{app.options.length}</p>
+            <button onClick={onRemoveAll}>Remove All</button>
+            <ol>
+                {
+                    app.options.map(option => {
+                        return (<li key={option}>Item {option}</li>)
+                    })
+                }
+            </ol>
+            <form onSubmit={onFormSubmit}>
+                <input type="text" name="option"/>
+                <button>Add Option</button>
+            </form>
+        </div>
+    );
 
-ReactDOM.render(template, document.getElementById('app'));
+    ReactDOM.render(template, appRoot);
+};
+
+render();
