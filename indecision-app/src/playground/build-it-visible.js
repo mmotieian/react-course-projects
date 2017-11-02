be6ba392f3675a934add063f713bc6ece90162ce
Mohammad Najar <mmotieian@yahoo.com>
2017-11-01 22:15:20 -0500

    visibility project

---
+++ indecision-app/src/playground/build-it-visible.js
@@ -0,0 +1,29 @@
+const appRoot = document.getElementById('app');
+
+
+let clicked = false;
+
+const showDetails = () => {
+    clicked = !clicked;
+    render();
+}
+
+
+const render = () => {
+    const template =
+        (
+            <div>
+                <h1>Visibility Toggle</h1>
+
+                <button onClick={showDetails}>{
+                    clicked ? "Hide Details" : "Show Details"
+                }</button>
+                {clicked && <p>Hello World</p>}
+            </div>
+        );
+
+    ReactDOM.render(template, appRoot);
+};
+
+
+render();
