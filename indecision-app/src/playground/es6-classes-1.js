5afed86bc481f1036c96086059b5203431669b08
Mohammad Najar <mmotieian@yahoo.com>
2017-11-03 20:35:41 -0500

    ES6 classes 2 practice

--- indecision-app/src/playground/es6-classes-1.js
+++ indecision-app/src/playground/es6-classes-1.js
@@ -1,4 +1,3 @@
-
 class Person {
 
 
@@ -7,24 +6,88 @@ class Person {
     // }
 
     // Or
-    constructor(name = 'test', age = 0){
-        this.name = name ;
+    constructor(name = 'test', age = 0) {
+        this.name = name;
         this.age = age;
     }
 
-    getGreeting(){
+    getGreeting() {
         return "Hi " + this.name + "!";
     }
 
-    getMyName(){
+    getDescription() {
+
         return `${this.name} is ${this.age} year(s) old.`;
     }
 }
 
-const me = new Person('Mohammad Najar', 33);
-console.log(me.getGreeting());
-console.log(me.getMyName());
+// -------------------------------------
 
-const other = new Person();
+class Student extends Person {
+    constructor(name, age, major) {
+        super(name, age);
+        this.major = major;
+    }
+
+    hasMajor() {
+        return !!this.major;
+    }
+
+    getDescription() {
+        let description = super.getDescription();
+
+        if (this.hasMajor()) {
+            description += ` Major is ${this.major}.`;
+        }
+
+        return description;
+    }
+}
+
+// const me = new Student('Mohammad Najar', 33, 'Computer Engineering');
+// console.log(me);
+// console.log(me.hasMajor());
+// console.log(me.getDescription());
+//
+// const other = new Student();
+// console.log(other);
+// console.log(other.hasMajor());
+// console.log(other.getDescription());
+
+// -------------------------------------
+
+class Traveler extends Person {
+
+    constructor(name, age, location) {
+        super(name, age);
+        this.location = location;
+    }
+
+    getGreeting() {
+
+        return `Hi. I am ${this.name}.`;
+    }
+
+
+    hasLocation() {
+        return !!this.location;
+    }
+
+    getDescription() {
+
+        let description = super.getDescription();
+        description += this.hasLocation() ? ` I'm visiting from ${this.location}.` : '';
+        return description;
+    }
+
+}
+
+
+const me = new Traveler('Mohammad Najar', 33, 'Dallas');
+console.log(me.getGreeting());
+console.log(me.getDescription());
+
+const other = new Traveler();
 console.log(other.getGreeting());
-console.log(other.getMyName());
+console.log(other.getDescription());
+
