3d0fa008292e1a90e6fcc1f39235c74336ca5a6e
Mohammad Najar <mmotieian@yahoo.com>
2017-11-06 23:06:00 -0600

    setting component states using setState

---
+++ indecision-app/src/playground/counter-example.js
@@ -0,0 +1,48 @@
+class Counter extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.handleAddOne = this.handleAddOne.bind(this);
+        this.handleMinusOne = this.handleMinusOne.bind(this);
+        this.handleReset = this.handleReset.bind(this);
+        this.state = {
+            count: 0
+        };
+    }
+
+    handleAddOne() {
+
+        this.state.count++;
+        this.setState(() => this.state);
+        console.log(this.state.count);
+    }
+
+    handleMinusOne() {
+
+        this.setState((prevState) => {
+
+            return {
+                count: prevState.count - 1
+            }
+        });
+    }
+
+    handleReset() {
+        this.state.count = 0;
+        this.setState(() => this.state);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Count: {this.state.count}</h1>
+                <button onClick={this.handleAddOne}>+1</button>
+                <button onClick={this.handleMinusOne}>-1</button>
+                <button onClick={this.handleReset}>reset</button>
+            </div>
+        );
+    }
+}
+
+
+ReactDOM.render(<Counter mytitle="hello"/>, document.getElementById('app'));
